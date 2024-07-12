b9f00e6a52f44002647824824ab767b35c8668f7
devmehmetakifv <go4webb@gmail.com>
2024-07-12 17:21:02 +0300

    Minor Layout logic and bug fix.

--- StudentInformationSystem/Controllers/HomeController.cs
+++ StudentInformationSystem/Controllers/HomeController.cs
@@ -1,27 +1,30 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using StudentInformationSystem.Business.Interfaces;
+using StudentInformationSystem.Entity.Concrete;
 using StudentInformationSystem.Models;
 using System.Diagnostics;
 
 namespace StudentInformationSystem.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
         private readonly ICourseService _courseService;
+        private readonly SignInManager<User> _signInManager;
 
-        public HomeController(ILogger<HomeController> logger, ICourseService courseService)
+        public HomeController(ILogger<HomeController> logger, ICourseService courseService, SignInManager<User> signInManager)
         {
             _logger = logger;
             _courseService = courseService;
+            _signInManager = signInManager;
         }
-
         public async Task<IActionResult> Index()
         {
-            var courses = await _courseService.GetAllCoursesAsync();
-            return View(courses);
+            return View();
         }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
